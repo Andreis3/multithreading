17fe3a61cd458e054eef2fcea67002913d779ddb
andreis3 <andrei.as3@hotmail.com>
2023-04-18 07:25:31 -0300

    wait group

--- 02-wait-groups/main.go
+++ 02-wait-groups/main.go
@@ -2,31 +2,36 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 // Thread 1
 func main() {
+	waitGroup := sync.WaitGroup{}
+	waitGroup.Add(25) // esperar as 25 operações terminarem
 	// Thread 2
-	go task("A")
+	go task("A", &waitGroup)
 	// Thread 3
-	go task("B")
+	go task("B", &waitGroup)
 	// Thread 4
 	go func() {
-		for i := 0; i < 10; i++ {
+		for i := 0; i < 5; i++ {
 			fmt.Printf("%d: Task %s is running\n", i, "anonymous")
 			time.Sleep(1 * time.Second)
+			waitGroup.Done()
 		}
 	}() // Thread anonima
 
 	// nada aqui
 	// sair do programa
-	time.Sleep(15 * time.Second) //segurar processo
+	waitGroup.Wait()
 }
 
-func task(name string) {
+func task(name string, waitGroup *sync.WaitGroup) {
 	for i := 0; i < 10; i++ {
 		fmt.Printf("%d: Task %s is running\n", i, name)
 		time.Sleep(1 * time.Second)
+		waitGroup.Done()
 	}
 }
